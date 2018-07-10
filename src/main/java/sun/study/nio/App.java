71d77c7df153eb3732d6356154dd5969ce6f9ff3
sunmengjie <2586354329@qq.com>
2018-07-10 23:02:41 +0800

    测试git用户

--- src/main/java/sun/study/nio/App.java
+++ src/main/java/sun/study/nio/App.java
@@ -1,5 +1,8 @@
 package sun.study.nio;
 
+import java.io.IOException;
+import java.nio.channels.SocketChannel;
+
 /**
  * @author Sun Mengjie
  * @version 2018/7/10-1
@@ -10,5 +13,10 @@ public class App {
     public static  void main(String args[]){
 
         System.out.println("java nio 学习demo程序");
+        try {
+            SocketChannel channel = SocketChannel.open();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
